ee6d518a966acaa971185bd500db22ad6c18ef7a
Chintan Patel <chintan97patel@gmail.com>
2018-02-14 11:32:07 +0530

    added feaures: search by category, subcategory

--- preview-products.php
+++ preview-products.php
@@ -19,7 +19,7 @@
 			<link rel="stylesheet" href="css/style-xlarge.css" />
 		</noscript>
 
-		<script>
+	<script>
 		function showResult(str) {
 		  if (str.length==0) { 
 		    document.getElementById("show_product").innerHTML="";
@@ -41,7 +41,74 @@
 		  xmlhttp.open("GET","search_product.php?q="+str,true);
 		  xmlhttp.send();
 		}
-		</script>
+
+		function showResult_cat(choice,str) {
+		  if (str.length==0) { 
+		    document.getElementById("show_product_cat").innerHTML="";
+		    document.getElementById("show_product_cat").style.border="0px";
+		    return;
+		  }
+		  if (window.XMLHttpRequest) {
+		    // code for IE7+, Firefox, Chrome, Opera, Safari
+		    xmlhttp=new XMLHttpRequest();
+		  } else {  // code for IE6, IE5
+		    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		  }
+		  xmlhttp.onreadystatechange=function() {
+		    if (this.readyState==4 && this.status==200) {
+		      document.getElementById("show_product_cat").innerHTML=this.responseText;
+		      document.getElementById("show_product_cat").style.border="1px solid #A5ACB2";
+		    }
+		  }
+		  xmlhttp.open("GET","search_product.php?q="+str+"&c="+choice,true);
+		  xmlhttp.send();
+		}
+
+		function showResult_subcat(choice,sub_choice,str) {
+		  if (str.length==0) { 
+		    document.getElementById("show_product_subcat").innerHTML="";
+		    document.getElementById("show_product_subcat").style.border="0px";
+		    return;
+		  }
+		  if (window.XMLHttpRequest) {
+		    // code for IE7+, Firefox, Chrome, Opera, Safari
+		    xmlhttp=new XMLHttpRequest();
+		  } else {  // code for IE6, IE5
+		    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		  }
+		  xmlhttp.onreadystatechange=function() {
+		    if (this.readyState==4 && this.status==200) {
+		      document.getElementById("show_product_subcat").innerHTML=this.responseText;
+		      document.getElementById("show_product_subcat").style.border="1px solid #A5ACB2";
+		    }
+		  }
+		  xmlhttp.open("GET","search_product.php?q="+str+"&c="+choice+"&s="+sub_choice,true);
+		  xmlhttp.send();
+		}
+
+		function change(){
+			if (document.getElementById('choice').value == 'by_product_only'){
+				document.getElementById('by_product_name').style.display='block';
+				document.getElementById('by_category').style.display='none';
+				document.getElementById('by_subcategory').style.display='none';
+			}
+			else if (document.getElementById('choice').value == 'by_category'){
+				document.getElementById('by_product_name').style.display='none';
+				document.getElementById('by_category').style.display='block';
+				document.getElementById('by_subcategory').style.display='none';
+			}
+			else{
+				document.getElementById('by_product_name').style.display='none';
+				document.getElementById('by_category').style.display='none';
+				document.getElementById('by_subcategory').style.display='block';
+			}
+		}
+
+		function get_choice(){
+			var choice = document.getElementById('choice_category').value;
+			document.getElementById('temp_cat_choice').value = choice;
+		}
+	</script>
 
 	</head>
 	<body>
@@ -89,10 +156,15 @@
 								$count_subcat = 0;  // subcategory count
 								$count_pro = 0;  // product count
 								$list = [];  // list format => [product_category, product_subcategory, product_name]
+								$unique_cat = ['']; // to use in drop down
+								$unique_subcat = [['','']]; // to use in drop down
+								$choice_to_fill = [];
 								foreach ($json_array[$_SESSION['shop_name']] as $key=>$value) {
 									$count_cat++;
+									array_push($unique_cat, $key);
 									foreach ($value as $key1 => $value1) {
 										$count_subcat++;
+										array_push($unique_subcat, [$key, $key1]);
 										foreach ($value1 as $key2 => $value2) {
 											$count_pro++;
 											$list_names = [];
@@ -109,15 +181,114 @@
 								$_SESSION["names"] = $list;
 								?>
 
-								<div align="left" id="by_product_name">
+								<strong>Select your 'search by' choice:</strong>
+								<div id="options" class="3u 6u$(4)">
+									<select id="choice" onchange="change();">
+										<option value="by_product_only">direct product name search</option>
+										<option value="by_category">category specific products</option>
+										<option value="by_subcategory">subcategory specific products</option>
+									</select>
+								</div>
+
+								<div align="left" id="by_product_name" style="display: block;">
 									<div class="3u 6u$(4)">
-										Search by product name: 
+										<br><strong>Search by product name: </strong>
 										<div align="left">
 										 <input type="text" id="search_product" name="search_product" onkeyup="showResult(this.value);">
 										 	<div id="show_product"></div>
 										 </div>
 									</div>
 								</div>
+
+								<div id="by_category" style="display: none;" class="3u 6u$(4)">
+									<br><strong>select category(will appear only if you have entered data for same)</strong>
+									<select id="choice_category" onchange="get_choice();">
+										<?php
+											foreach ($unique_cat as $key => $value) {
+												echo "<option value='".$value."'>".$value."</option>";	
+											}
+										?>
+									</select>
+									<input type="hidden" id="temp_cat_choice">
+									<br><strong>Product name</strong>
+									<input type="text" id="search_product" name="search_product" onkeyup="showResult_cat(document.getElementById('temp_cat_choice').value,this.value);">
+									<div id="show_product_cat"></div>
+								</div>
+
+								<script>
+									var choices = new Array();
+									var i = 0;
+
+									<?php foreach ($unique_subcat as $value) { ?>
+											var temp = [];
+											temp.push('<?php echo $value[0]; ?>');
+											temp.push('<?php echo $value[1]; ?>');
+											choices.push(temp);
+											//console.log(choices);
+											i++;
+											//document.write(choices);
+									<?php }  ?>
+
+									function get_choice_sub(){
+										var selected_cat = document.getElementById('choice_subcategory').value;	
+										removeAllOptions(document.temp_form.fill_subcategory_choices);
+
+										for (var j=0; j<choices.length; j++){
+											if (selected_cat == choices[j][0]){
+												addOption(document.temp_form.fill_subcategory_choices, choices[j][1], choices[j][1]);
+											}
+										}
+									}
+
+									function addOption(selectbox, value, text )
+									{
+										var optn = document.createElement("OPTION");
+										optn.text = text;
+										optn.value = value;
+
+										selectbox.options.add(optn);
+									}
+
+									function removeAllOptions(selectbox)
+									{
+										var i;
+										for(i=selectbox.options.length-1;i>=0;i--)
+										{
+												//selectbox.options.remove(i);
+												selectbox.remove(i);
+										}
+									}
+
+									function set_subcategory(){
+										var temp_cat_name = document.getElementById('choice_subcategory').value;
+										var temp_subcat_name = document.getElementById('fill_subcategory_choices').value;
+										document.getElementById('temp_cat_choice_sub').value = temp_cat_name;
+										document.getElementById('temp_subcat_choice_sub').value = temp_subcat_name;
+									}
+								</script>
+
+
+								<div id="by_subcategory" style="display: none;" class="3u 6u$(4)">
+									<br><strong>select category(will appear only if you have entered data for same)</strong>
+									<select id="choice_subcategory" onchange="get_choice_sub();">
+										<?php
+											foreach ($unique_cat as $key => $value) {
+												echo "<option value='".$value."''>".$value."</option>";	
+											}
+										?>
+									</select>
+									<br><strong>select subcategory(will appear only if you have entered data for same)</strong>
+									<form name="temp_form">
+									<select id="fill_subcategory_choices" name="fill_subcategory_choices" onchange="set_subcategory();">
+									</select>
+									</form>
+									<input type="hidden" id="temp_cat_choice_sub">
+									<input type="hidden" id="temp_subcat_choice_sub">
+									<br><strong>Product name</strong>
+									<input type="text" id="search_product" name="search_product" onkeyup="showResult_subcat(document.getElementById('choice_subcategory').value,document.getElementById('fill_subcategory_choices').value,this.value);">
+									<div id="show_product_subcat"></div>
+								</div>
+						
 								<?php
 
 								if (isset($_GET["pro"])){
